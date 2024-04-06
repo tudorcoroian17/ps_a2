b3b6bd40e745419c3c68004862806169b4c730bf
Tudor Coroian <tudorcoroian17w@gmail.com>
2024-04-06 11:51:44 +0300

    Initial commit

---
+++ src/main/java/io/mgherman/asgn2/Asgn2Application.java
@@ -0,0 +1,13 @@
+package io.mgherman.asgn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asgn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asgn2Application.class, args);
+	}
+
+}
